8fc7b86551f9cb74d8abcf4709a8a0a1ada41c2c
github-lx <github-lx@snkmail.com>
2026-05-10 08:14:39 -0400

    doc(samples/interrupt): show URL-decoded initial_state in curl-run.sh and remove unneeded space character

--- samples/interrupt/curl-run.sh
+++ samples/interrupt/curl-run.sh
@@ -1,6 +1,7 @@
 curl \
   -v \
   -X POST \
-  'http://127.0.0.1:8000/run?initial_state=%7B%22value%22%3A%2042%7D'
+  'http://127.0.0.1:8000/run?initial_state=%7B%22value%22%3A42%7D'
+#                                          {    "value"  :  42}
 
 echo
